5b12f817ead12a0f10f0c4f6ff18208ad4028cc6
simon <aodtorusan@gmail.com>
2011-10-09 16:27:39 +0200

    Added some missing license notices

--- src/main/interfaces/be/angelcorp/libs/celest/stateVector/ICartesianElements.java
+++ src/main/interfaces/be/angelcorp/libs/celest/stateVector/ICartesianElements.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 simon <aodtorusan@gmail.com>
+ *
+ * Licensed under the Non-Profit Open Software License version 3.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.opensource.org/licenses/NOSL3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package be.angelcorp.libs.celest.stateVector;
 
 import be.angelcorp.libs.celest.math.Cartesian;
