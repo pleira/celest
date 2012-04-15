2a1a4c90f59ad4d902426c7b86874a3e30bf921b
Simon Billemont <aodtorusan@gmail.com>
2012-04-15 19:12:12 +0200

    mend

--- src/test/java/be/angelcorp/libs/celest/maneuvers/targeters/exposin/ExpoSinTest.java
+++ src/test/java/be/angelcorp/libs/celest/maneuvers/targeters/exposin/ExpoSinTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009-2012 simon <simon@angelcorp.be>
+ *
+ * Licensed under the Non-Profit Open Software License version 3.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.opensource.org/licenses/NOSL3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package be.angelcorp.libs.celest.maneuvers.targeters.exposin;
 
 import static org.apache.commons.math3.util.FastMath.PI;
@@ -18,7 +33,7 @@ import be.angelcorp.libs.util.physics.Time;
  * <a href="http://www.mathworks.com/matlabcentral/fileexchange/29272">Skipping Stone - An interplanetary
  * space mission design tool</a><br />
  * <b>Rody P. Oldenhuis, "Trajectory optimization of a mission to the solar bow shock and minor planets",
- * Master’s Thesis, Delft University of Technology, 2010.</b>
+ * Masterï¿½s Thesis, Delft University of Technology, 2010.</b>
  * </p>
  * 
  * @author Simon Billemont
