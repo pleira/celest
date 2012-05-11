5ae739cfd9c2fcc315fe633d12ccf1eade294441
simon <simon@angelcorp.be>
2012-05-11 16:58:57 +0200

    Added IFrameTransform and (still empty) IReferenceFrame

---
+++ src/main/interfaces/be/angelcorp/libs/celest/frames/IFrameTransform.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (C) 2009-2012 simon <simon@angelcorp.be>
+ *
+ * Licensed under the Non-Profit Open Software License version 3.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.opensource.org/licenses/NOSL3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.angelcorp.libs.celest.frames;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+
+import be.angelcorp.libs.celest.state.orientationState.IOrientationState;
+import be.angelcorp.libs.celest.state.positionState.IPositionState;
+import be.angelcorp.libs.celest.state.positionState.IPositionStateDerivative;
+import be.angelcorp.libs.math.linear.Vector3D;
+
+/**
+ * Transform between different {@link IReferenceFrame}'s.
+ * 
+ * @author Simon Billemont
+ */
+public interface IFrameTransform {
+
+	/**
+	 * Get a new transform that, when applied, undoes the effects performed by this transformation.
+	 * This means:
+	 * 
+	 * <pre>
+	 * v = this.inverse().transform(this.transform(v))
+	 * </pre>
+	 * 
+	 * @return An inverse transformation of this {@link IFrameTransform}
+	 */
+	public abstract IFrameTransform inverse();
+
+	/**
+	 * Transform a {@link IOrientationState} into a new {@link IReferenceFrame}.
+	 * 
+	 * @param orientationState
+	 *            Orientation to transform.
+	 * @return A new {@link IOrientationState} which is equivalent to given
+	 *         {@link IOrientationState}, but in the new {@link IReferenceFrame}.
+	 */
+	public abstract IPositionStateDerivative transform(IOrientationState orientationState);
+
+	/**
+	 * Transform a {@link IPositionState} into a new {@link IReferenceFrame}.
+	 * 
+	 * @param positionState
+	 *            Position to transform.
+	 * @return A new {@link IPositionState} which is equivalent to given {@link IPositionState}, but
+	 *         in the new {@link IReferenceFrame}.
+	 */
+	public abstract IPositionState transform(IPositionState positionState);
+
+	/**
+	 * Transform a {@link IPositionStateDerivative} into a new {@link IReferenceFrame}.
+	 * 
+	 * @param positionStateDerivative
+	 *            Position derivatives to transform.
+	 * @return A new {@link IPositionStateDerivative} which is equivalent to given
+	 *         {@link IPositionStateDerivative}, but in the new {@link IReferenceFrame}.
+	 */
+	public abstract IPositionStateDerivative transform(IPositionStateDerivative positionStateDerivative);
+
+	/**
+	 * Transform only the acceleration of a body into a new {@link IReferenceFrame}.
+	 * 
+	 * <pre>
+	 * acceleration = { a<sub>x</sub>, a<sub>y</sub>, a<sub>z</sub> }
+	 * </pre>
+	 * 
+	 * <p>
+	 * <b>Unit: [m/s&#178;]</b>
+	 * </p>
+	 * 
+	 * @param acceleration
+	 *            Acceleration to transform.
+	 * @returnA new acceleration vector which is equivalent to given provided vector, but in the new
+	 *          {@link IReferenceFrame}.
+	 */
+	public abstract Vector3D transformAcceleration(Vector3D acceleration);
+
+	/**
+	 * Transform only the orientation of a body into a new {@link IReferenceFrame}.
+	 * 
+	 * @param orientation
+	 *            Orientation to transform.
+	 * @returnA new orientation which is equivalent to given provided orientation, but in the new
+	 *          {@link IReferenceFrame}.
+	 */
+	public abstract Rotation transformOrientation(Rotation orientation);
+
+	/**
+	 * Transform only the orientation rate of a body into a new {@link IReferenceFrame}.
+	 * 
+	 * <pre>
+	 * orientationRate = { d&omega;<sub>x</sub>, d&omega;<sub>y</sub>, d&omega;<sub>z</sub> }
+	 * </pre>
+	 * 
+	 * <p>
+	 * <b>Unit: [rad]</b>
+	 * </p>
+	 * 
+	 * @param orientationRate
+	 *            Orientation rate to transform.
+	 * @returnA new orientation rate which is equivalent to given provided orientation rate, but in
+	 *          the new {@link IReferenceFrame}.
+	 */
+	public abstract Vector3D transformOrientationRate(Vector3D orientationRate);
+
+	/**
+	 * Transform only the position of a body into a new {@link IReferenceFrame}.
+	 * 
+	 * <pre>
+	 * position= { x, y, z }
+	 * </pre>
+	 * 
+	 * <p>
+	 * <b>Unit: [m]</b>
+	 * </p>
+	 * 
+	 * @param position
+	 *            Position to transform.
+	 * @returnA new position vector which is equivalent to given provided vector, but in the new
+	 *          {@link IReferenceFrame}.
+	 */
+	public abstract Vector3D transformPosition(Vector3D position);
+
+	/**
+	 * Transform only the velocity of a body into a new {@link IReferenceFrame}.
+	 * 
+	 * <pre>
+	 * velocity = { v<sub>x</sub>, v<sub>y</sub>, v<sub>z</sub> }
+	 * </pre>
+	 * 
+	 * <p>
+	 * <b>Unit: [m/s]</b>
+	 * </p>
+	 * 
+	 * @param velocity
+	 *            Velocity to transform.
+	 * @returnA new velocity vector which is equivalent to given provided vector, but in the new
+	 *          {@link IReferenceFrame}.
+	 */
+	public abstract Vector3D transformVelocity(Vector3D velocity);
+
+}
