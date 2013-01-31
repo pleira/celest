1532ed7d48bc374a940f6b479f77d3e9face76a7
Simon Billemont <aodtorusan@gmail.com>
2013-01-31 15:11:27 +0100

    Added a better Lambert targetter for high-thrust (2-body) solutions

---
+++ src/main/java/be/angelcorp/libs/celest/maneuvers/targeters/lambert/LambertTrajectory2.scala
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2013 Simon Billemont <simon@angelcorp.be>
+ *
+ * Licensed under the Non-Profit Open Software License version 3.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.opensource.org/licenses/NOSL3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.angelcorp.libs.celest.maneuvers.targeters.lambert
+
+import be.angelcorp.libs.celest.trajectory.{KeplerTrajectory, ITrajectory}
+import be.angelcorp.libs.celest.time.IJulianDate
+import be.angelcorp.libs.celest.state.positionState.{KeplerElements, IPositionState}
+import be.angelcorp.libs.celest.body.CelestialBody
+
+/**
+ * The solution trajectory from a basic lambert problem (TPBVP). It consists of the full state vector at the
+ * departure point and arrival point. The trajectory between the two points is a basic unperturbed two-body
+ * Keplerian motion.
+ *
+ * @param origin State vector at the origin point.
+ * @param destination State vector at the destination.
+ * @param departure Departure time from the origin.
+ * @param arrival Arrival time at the destination.
+ * @param center Center body around which the trajectory takes place.
+ */
+class LambertTrajectory2(val origin: IPositionState,
+												 val destination: IPositionState,
+												 val departure: IJulianDate,
+												 val arrival: IJulianDate,
+												 val center: CelestialBody) extends KeplerTrajectory( KeplerElements.as(origin, center), departure );
--- src/test/java/be/angelcorp/libs/celest/CelestMatchers.scala
+++ src/test/java/be/angelcorp/libs/celest/CelestMatchers.scala
@@ -23,13 +23,17 @@ import org.apache.commons.math3.util.Precision
 
 trait CelestMatchers {
 
-	sealed abstract class VectorTolerance
-	case class VectorEqualTolerance() extends VectorTolerance
-	{ override def toString = "0" }
-	case class VectorDoubleTolerance(eps: Double)   extends VectorTolerance
-	{ override def toString = eps.toString }
-	case class VectorVectorTolerance(eps: Vector3D) extends VectorTolerance
-	{ override def toString = eps.toString }
+	sealed abstract class VectorTolerance {
+		def check(left: Double, right: Double): Boolean
+	}
+	case class VectorEqualTolerance() extends VectorTolerance {
+		override def check(left: Double, right: Double) = Precision.equals(left, right)
+		override def toString = "0"
+	}
+	case class VectorDoubleTolerance(eps: Double)   extends VectorTolerance {
+		override def check(left: Double, right: Double) = Precision.equals(left, right, eps)
+		override def toString = eps.toString
+	}
 
 	def <[T <% Ordered[T]](right: T): Matcher[T] =
 		new Matcher[T] {
@@ -41,34 +45,32 @@ trait CelestMatchers {
 				)
 		}
 
-	def equalVector[T](right: Vector3D, tolerance: VectorTolerance): Matcher[T] =
-		new Matcher[T] {
-			def apply(left: T): MatchResult = {
-				val matches = tolerance match {
-					case VectorEqualTolerance() => false
-						//Precision.equals(left.x, right.x) &&
-						//Precision.equals(left.y, right.y) &&
-						//Precision.equals(left.z, right.z)
-					case VectorDoubleTolerance(eps) => false
-						//Precision.equals(left.x, right.x, eps) &&
-						//Precision.equals(left.y, right.y, eps) &&
-						//Precision.equals(left.z, right.z, eps)
-					case VectorVectorTolerance(eps) => false
-						//Precision.equals(left.x, right.x, eps.x) &&
-						//Precision.equals(left.y, right.y, eps.y) &&
-						//Precision.equals(left.z, right.z, eps.z)
-				}
+	def equalVector(right: Vector3D, tolerance: VectorTolerance): Matcher[Vector3D] =
+		new Matcher[Vector3D] {
+			def apply(left: Vector3D) = {
+				val (matches, errors) = left.zip(right).zipWithIndex.foldLeft( (true, "") )( (previous, entries) =>
+					tolerance.check( entries._1._1, entries._1._2 ) match {
+						case true  => previous
+						case false => (false, previous._2 + "\nIndex %d not equal: %16.16f <> %16.16f within %s".format(entries._2, entries._1._1, entries._1._2, tolerance))
+				} )
 				MatchResult(
 					matches,
-					"the vector" + right + " is not equal to " + left + " within absolute tolerance " + tolerance,
-					"the vector" + right + " is equal to " + left + " within absolute tolerance " + tolerance
+					"the vector" + right + " is not equal to " + left + " within absolute tolerance " + tolerance + errors,
+					"the vector" + right + " is equal to " + left + " within absolute tolerance " + tolerance + errors
 				)
 			}
 		}
-	def equalVector(right: Vector3D):                      Matcher[Vector3D] = equalVector(right, VectorEqualTolerance())
-	def equalVector(right: Vector3D, tolerance: Double):   Matcher[Vector3D] = equalVector(right, VectorDoubleTolerance(tolerance))
-	def equalVector(right: Vector3D, tolerance: Vector3D): Matcher[Vector3D] = equalVector(right, VectorVectorTolerance(tolerance))
+	def vector(right: Vector3D):                      Matcher[Vector3D] = equalVector(right, VectorEqualTolerance())
+	def vector(right: Vector3D, tolerance: Double):   Matcher[Vector3D] = equalVector(right, VectorDoubleTolerance(tolerance))
 
+	def assertVectorEqual(left: Vector3D, right: Vector3D) =
+		vector(right).apply(left) match {
+			case MatchResult(success, failureMessage, n, m, mn) => assert(success, failureMessage)
+		}
+	def assertVectorEqual(left: Vector3D, right: Vector3D, tolerance: Double) =
+		vector(right, tolerance).apply(left) match {
+			case MatchResult(success, failureMessage, n, m, mn) => assert(success, failureMessage)
+		}
 }
 
 object CelestMatchers extends CelestMatchers
