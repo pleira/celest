25edaa13520c657c0c070eecf17a233661d06bdb
Simon Billemont <simon@angelcorp.be>
2014-03-17 17:14:35 +0100

    Created a builder for a Universe (a point where configuration modules can still be added/removed)

--- src/main/resources/snippets/universe/injector.scala
+++ src/main/resources/snippets/universe/injector.scala
@@ -1,19 +1,20 @@
-import be.angelcorp.celest.universe.DefaultUniverse
-import com.google.inject.Module
+import be.angelcorp.celest.universe.DefaultUniverseBuilder
 import net.codingwell.scalaguice.ScalaModule
 
 // Define a basic class
 class MyClass(val message: String)
 
-implicit val universe = new DefaultUniverse {
-  override def configurationModules: Iterable[Module] = super.configurationModules ++ Seq(
-    // Add a new module to the injector configuration to create a binding for MyClass
-    new ScalaModule {
-      // Create a link between the MyClass class and an implementation of that class
-      def configure() = bind[MyClass].toInstance(new MyClass("Hello injector"))
-    }
-  )
+// Create a builder for the universe, in this object all the wiring for the final universe takes  place.
+val builder = new DefaultUniverseBuilder {
+  // Add a new module to the injector configuration to create a binding for MyClass
+  modules += new ScalaModule {
+    // Create a link between the MyClass class and an implementation of that class
+    def configure() = bind[MyClass].toInstance(new MyClass("Hello injector"))
+  }
 }
+
+// Create the actual universe
+implicit val universe = builder.result
 
 // Print the message of the MyClass instance held by the injector
 println(universe.instance[MyClass].message)
